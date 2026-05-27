b35e164c7b54daeba6eb24cf8e019bd9911e8735
hana891109 <baaa891109@gmail.com>
2026-05-27 14:43:30 +0800

    Update server.py

--- server.py
+++ server.py
@@ -262,11 +262,11 @@ class Handler(BaseHTTPRequestHandler):
         else:
             self.send_response(404); self.end_headers()
     def do_OPTIONS(self):
-    self.send_response(200)
-    self.send_header("Access-Control-Allow-Origin","*")
-    self.send_header("Access-Control-Allow-Methods","GET, OPTIONS")
-    self.send_header("Access-Control-Allow-Headers","*")
-    self.end_headers()
+        self.send_response(200)
+        self.send_header("Access-Control-Allow-Origin","*")
+        self.send_header("Access-Control-Allow-Methods","GET, OPTIONS")
+        self.send_header("Access-Control-Allow-Headers","*")
+        self.end_headers()
 
 class ThreadedServer(socketserver.ThreadingMixIn, HTTPServer): pass
 
